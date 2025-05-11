54235cc1b8f77c4346f2c868631ce92e37583378
Teejayssmart <teejaysmarty@gmail.com>
2025-05-11 21:05:23 +0100

    Resolved merge conflicts

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
 
+<<<<<<< HEAD
 urlpatterns = [
    
     path('', views.posts_list),
+=======
+app_name = 'posts'
+
+urlpatterns = [
+   
+    path('', views.posts_list, name = "list"),
+    path('<slug:slug>', views.post_page, name="page"),
+>>>>>>> dfe5f18 (creating admin panel)
     
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Post
 
+<<<<<<< HEAD
 
 # Create your views here.
 
 def posts_list(request):
   posts = Post.objects.all().order_by('-date')
   return render(request, 'posts/posts_list.html', {'posts' : posts})
+=======
+# Create your views here.
+
+
+def posts_list(request):
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
+>>>>>>> dfe5f18 (creating admin panel)
